46e6781c1f2ef04a4725b721d34e4b2d26d26f17
nicolas balao <nicolasbalao.pro@gmail.com>
2024-12-17 10:58:02 +0100

    feat(backend): Config db + health check for the app

--- backend/start/routes.ts
+++ backend/start/routes.ts
@@ -8,9 +8,12 @@
 */
 
 import router from '@adonisjs/core/services/router'
+const HealthChecksController = () => import('#controllers/health_checks_controller')
 
 router.get('/', async () => {
   return {
     hello: 'world',
   }
 })
+
+router.get('/health', [HealthChecksController])
