6d266c98857a1283d773418f148054b09c632805
nicolas balao <nicolasbalao.pro@gmail.com>
2024-12-18 14:29:31 +0100

    ref(back): Delete default route

--- backend/start/routes.ts
+++ backend/start/routes.ts
@@ -10,10 +10,4 @@
 import router from '@adonisjs/core/services/router'
 const HealthChecksController = () => import('#controllers/health_checks_controller')
 
-router.get('/', async () => {
-  return {
-    hello: 'world',
-  }
-})
-
 router.get('/health', [HealthChecksController])
