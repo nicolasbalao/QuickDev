b454d9a10a53a4557fcc22573bbc20ba16df5ecb
nicolas balao <nicolasbalao.pro@gmail.com>
2024-12-19 14:19:44 +0100

    feat(web): Add 'Logo' for going back to home page

--- web/src/components/TheHeader.vue
+++ web/src/components/TheHeader.vue
@@ -9,21 +9,27 @@ const visible = ref(false)
 
 <template>
   <header
-    class="flex items-center justify-end border-b border-solid bg-surface-50 p-5 dark:border-surface-700 dark:bg-surface-950"
+    class="flex items-center justify-between border-b border-solid bg-surface-50 p-5 dark:border-surface-700 dark:bg-surface-950"
   >
-    <button
-      class="mr-8 flex items-center gap-2 rounded-lg border border-solid p-2 text-sm text-slate-600 shadow-sm dark:border-primary dark:text-primary"
-      @click="visible = true"
-    >
-      <span class="pi pi-folder-plus text-slate-600 dark:text-primary"></span>
-      <span>New project</span>
-    </button>
-    <DarkModeToggle />
-    <Dialog v-model:visible="visible" modal header="Create new project">
-      <ProjectFormCreation
-        @onCancel="visible = false"
-        @onSuccess="visible = false"
-      ></ProjectFormCreation>
-    </Dialog>
+    <div class="ml-4">
+      <RouterLink :to="{ name: 'home' }" class="text-xl">QuickDev</RouterLink>
+    </div>
+
+    <div class="flex items-center">
+      <button
+        class="mr-8 flex items-center gap-2 rounded-lg border border-solid p-2 text-sm text-slate-600 shadow-sm dark:border-primary dark:text-primary"
+        @click="visible = true"
+      >
+        <span class="pi pi-folder-plus text-slate-600 dark:text-primary"></span>
+        <span>New project</span>
+      </button>
+      <DarkModeToggle />
+      <Dialog v-model:visible="visible" modal header="Create new project">
+        <ProjectFormCreation
+          @onCancel="visible = false"
+          @onSuccess="visible = false"
+        ></ProjectFormCreation>
+      </Dialog>
+    </div>
   </header>
 </template>
