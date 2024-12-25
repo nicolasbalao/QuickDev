c9c55e0b4564acd5d6061d20089c5f2299062938
nicolas balao <nicolasbalao.pro@gmail.com>
2024-12-25 16:33:52 +0100

    ref(project): Make repo_url and api_repo_url to be unique

--- backend/database/migrations/1734529258656_create_projects_table.ts
+++ backend/database/migrations/1734529258656_create_projects_table.ts
@@ -11,8 +11,8 @@ export default class extends BaseSchema {
       table.string('slug', 50).notNullable().unique()
       table.string('path').notNullable()
       table.text('description').defaultTo('')
-      table.string('repo_url')
-      table.string('api_repo_url')
+      table.string('repo_url').unique()
+      table.string('api_repo_url').unique()
       table.boolean('public_repo')
 
       table.integer('template_id').unsigned().references('project_templates.id')
