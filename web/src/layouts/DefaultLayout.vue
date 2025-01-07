f0a18c0d460667abe15be322763296dafd6febf9
nicolas balao <nicolasbalao.pro@gmail.com>
2025-01-07 18:00:41 +0100

    ref(layout): Change behaviour with height and scroll

--- web/src/layouts/DefaultLayout.vue
+++ web/src/layouts/DefaultLayout.vue
@@ -4,12 +4,14 @@ import TheSideBar from '../components/TheSideBar.vue'
 </script>
 
 <template>
-  <main class="flex h-screen w-screen flex-col">
+  <main class="flex h-screen w-screen flex-col overflow-y-hidden">
     <TheHeader />
     <div class="flex flex-1 gap-8">
       <TheSideBar class="" />
-      <div class="mr-6 mt-8 flex-grow">
-        <slot></slot>
+      <div class="max-h-screen w-full overflow-y-scroll">
+        <div class="h-full w-full pb-2 pr-6 pt-8">
+          <slot></slot>
+        </div>
       </div>
     </div>
   </main>
