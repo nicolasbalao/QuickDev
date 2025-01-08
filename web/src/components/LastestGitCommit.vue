6bd8429ef157826888f6981d987afdecb41deb01
nicolas balao <nicolasbalao.pro@gmail.com>
2025-01-08 15:40:23 +0100

    feat(commit_component): Add message when commits is empty

--- web/src/components/LastestGitCommit.vue
+++ web/src/components/LastestGitCommit.vue
@@ -11,43 +11,49 @@ defineProps<{ commits: GitCommit[] }>()
     class="flex flex-col gap-2 rounded-md border border-solid border-surface-100 p-4 dark:border-surface-700"
   >
     <span class="text-sm font-semibold">Latest changes</span>
-    <Timeline :value="Object.values(commits)">
-      <template #content="slotProps">
-        <div class="flex flex-col gap-1">
-          <span class="text-sm text-surface-400">{{ timeAgo(slotProps.item.date) }} ago</span>
+    <!-- TODO change this v-if -->
+    <template v-if="commits.length > 0">
+      <Timeline :value="Object.values(commits)">
+        <template #content="slotProps">
+          <div class="flex flex-col gap-1">
+            <span class="text-sm text-surface-400">{{ timeAgo(slotProps.item.date) }} ago</span>
 
-          <a
-            v-if="slotProps.item.url"
-            :href="slotProps.item.url"
-            v-tooltip.bottom="{
-              value: slotProps.item.message,
-              showDelay: 500,
-              pt: {
-                text: '!text-sm',
-              },
-            }"
-            class="line-clamp-2 text-left text-sm text-surface-600 hover:cursor-pointer hover:underline dark:text-surface-300"
-            >{{ slotProps.item.message }}</a
-          >
-          <span
-            v-else
-            v-tooltip.bottom="{
-              value: slotProps.item.message,
-              showDelay: 500,
-              pt: {
-                text: '!text-sm',
-              },
-            }"
-            class="line-clamp-2 text-left text-sm text-surface-600 dark:text-surface-300"
-          >
-            {{ slotProps.item.message }}
-          </span>
-          <span class="text-xs text-surface-400 dark:text-surface-400">{{
-            slotProps.item.author
-          }}</span>
-        </div>
-      </template>
-    </Timeline>
+            <a
+              v-if="slotProps.item.url"
+              :href="slotProps.item.url"
+              v-tooltip.bottom="{
+                value: slotProps.item.message,
+                showDelay: 500,
+                pt: {
+                  text: '!text-sm',
+                },
+              }"
+              class="line-clamp-2 text-left text-sm text-surface-600 hover:cursor-pointer hover:underline dark:text-surface-300"
+              >{{ slotProps.item.message }}</a
+            >
+            <span
+              v-else
+              v-tooltip.bottom="{
+                value: slotProps.item.message,
+                showDelay: 500,
+                pt: {
+                  text: '!text-sm',
+                },
+              }"
+              class="line-clamp-2 text-left text-sm text-surface-600 dark:text-surface-300"
+            >
+              {{ slotProps.item.message }}
+            </span>
+            <span class="text-xs text-surface-400 dark:text-surface-400">{{
+              slotProps.item.author
+            }}</span>
+          </div>
+        </template>
+      </Timeline>
+    </template>
+    <template v-else>
+      <span>No commits </span>
+    </template>
   </div>
 </template>
 
